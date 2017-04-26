424a7d442e12e2f787e4d951448552b9c15cab98
Ionică Bizău <bizauionica@gmail.com>
2017-04-26 11:56:47 +0300

    Support

--- example/index.js
+++ example/index.js
@@ -10,3 +10,6 @@ console.log(formatoid(d, "MMMM D, YYYY"));
 
 console.log(formatoid(d, "dddd, MMMM, D, YYYY h:m A"));
 // => Wednesday, December, 20, 1989 12:0 AM
+
+console.log(formatoid(d, "YYYYMMDDT0HHMMSSZ"));
+// => 19891221T0001200+02:00
